0a0acb62e62d2b8db91e15e591b5594455fd912e
Vincenwwx <berneywwx@gmail.com>
2022-02-13 21:18:22 +0100

    Add raft 1.0

---
+++ consensus/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	. "consensus/raft"
+	"net"
+)
+
+func main() {
+	ready := make(chan interface{})
+	server := NewServer(22, []int{33, 44}, ready)
+	server.Serve()
+
+	server.ConnectToPeer(33, net.JoinHostPort("192.168.0.208", "8888"))
+	server.ConnectToPeer(44, net.JoinHostPort("192.168.0.208", "8888"))
+
+	close(ready)
+}
